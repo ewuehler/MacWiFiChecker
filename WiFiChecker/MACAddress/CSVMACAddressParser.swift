0f49c66d7b8b5501c38eee0e8925ae9f1fbda953
Eric Wuehler <ewuehler@gmail.com>
2019-01-15 21:54:58 -0800

    added mac addr by file rather than API lookup

--- WiFiChecker/MACAddress/CSVMACAddressParser.swift
+++ WiFiChecker/MACAddress/CSVMACAddressParser.swift
@@ -28,17 +28,31 @@ class CSVMACAddressParser {
     func orgName(by mac: String) -> String? {
         let raw = mac.replacingOccurrences(of: ":", with: "", options: NSString.CompareOptions.literal, range:nil)
         let oui24: String = String(raw.dropLast(6))
-        print(oui24)
-        let m = mal[oui24]
-        if m != nil {
-            return m?.OrgName
+        let oui28: String = String(raw.dropLast(5))
+        let oui36: String = String(raw.dropLast(3))
+//        print("Small: \(oui36), Medium: \(oui28), Large: \(oui24)")
+        let m36 = mas[oui36]
+        if m36 != nil {
+//            print("Small: \(String(describing: m36))")
+            return m36?.OrgName
+        }
+        let m28 = mam[oui28]
+        if m28 != nil {
+//            print("Medium: \(String(describing: m28))")
+            return m28?.OrgName
+        }
+        let m24 = mal[oui24]
+        if m24 != nil {
+            return m24?.OrgName
         } else {
-            return nil
+            return "[Unknown]"
         }
     }
     
     
     private func load() {
+        // TODO: maybe pull these down in preferences once a week or on demand
+        // and not have them embedded in the binary...
         load(path: Bundle.main.path(forResource: "mal", ofType: "csv")!, which: &mal)
         load(path: Bundle.main.path(forResource: "mam", ofType: "csv")!, which: &mam)
         load(path: Bundle.main.path(forResource: "mas", ofType: "csv")!, which: &mas)
@@ -46,15 +60,18 @@ class CSVMACAddressParser {
     
     private func load(path: String, which: inout [String:MACAddress]) {
         do {
-            let data = try String(contentsOfFile: path, encoding: .utf8).replacingOccurrences(of: "\r\n", with: "\n")
+            let data = try String(contentsOfFile: path, encoding: .utf8) //.replacingOccurrences(of: "\r\n", with: "\n")
             let lines = data.components(separatedBy: .newlines)
             for line in lines {
-                let result = split(line, separator:",")
-                if (result.count != 4) {
-                    print("Invalid # of results from the split: \(result)")
-                } else {
-                    let m = MACAddress(registry: result[0], assignment: result[1], orgName: result[2], orgAddress: result[3])
-                    which[m.Assignment] = m
+                let tline = line.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+                if (tline.count > 0) {
+                    let result = split(tline, separator:",")
+                    if (result.count != 4) {
+                        print("Invalid # of results from the split: \(result)")
+                    } else {
+                        let m = MACAddress(registry: result[0], assignment: result[1], orgName: result[2], orgAddress: result[3])
+                        which[m.Assignment] = m
+                    }
                 }
             }
         } catch {
--- WiFiChecker/MACAddress/MACAddress.swift
+++ WiFiChecker/MACAddress/MACAddress.swift
@@ -15,9 +15,24 @@ class MACAddress {
     var OrgAddress: String
     
     init(registry: String, assignment: String, orgName: String, orgAddress: String) {
+        
+        func normalize(_ str: String) -> String {
+            var out: String = str
+            if (out.hasPrefix("\"")) {
+                let start = out.index(out.startIndex, offsetBy: 1)
+                out = String(out[start...])
+                if (out.hasSuffix("\"")) {
+                    let end = out.index(out.endIndex, offsetBy:-1)
+                    out = String(out[..<end])
+                }
+            }
+            return out
+        }
+        
         self.Registry = registry
         self.Assignment = assignment
-        self.OrgName = orgName
-        self.OrgAddress = orgAddress
+        self.OrgName = normalize(orgName)
+        self.OrgAddress = normalize(orgAddress)
     }
+    
 }
